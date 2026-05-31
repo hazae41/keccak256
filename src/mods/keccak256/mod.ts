4fa01f368d0b87ec39667745cea944408133b9a4
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-31 23:04:37 +0700

    f

--- src/mods/keccak256/mod.ts
+++ src/mods/keccak256/mod.ts
@@ -2,7 +2,7 @@ import { sha3Wasm } from "@hazae41/sha3-wasm";
 
 await sha3Wasm.load()
 
-export function digest(payload: Uint8Array) {
+export function digest(payload: Uint8Array): Uint8Array<ArrayBuffer> {
   const { Memory } = sha3Wasm
 
   const result = sha3Wasm.keccak256(new Memory(payload))
@@ -16,11 +16,11 @@ export class Hasher {
     readonly inner = new sha3Wasm.Keccak256Hasher()
   ) { }
 
-  clone() {
+  clone(): Hasher {
     return new Hasher(this.inner.clone())
   }
 
-  update(data: Uint8Array) {
+  update(data: Uint8Array): this {
     const { Memory } = sha3Wasm
 
     this.inner.update(new Memory(data))
@@ -28,7 +28,7 @@ export class Hasher {
     return this
   }
 
-  finalize() {
+  finalize(): Uint8Array<ArrayBuffer> {
     return new Uint8Array(this.inner.finalize().bytes)
   }
 
